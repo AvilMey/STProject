460c2b317899e63e3bf9cf9e24b3535fbeb42f28
AvilMey <m.andromeda98@gmail.com>
2023-08-11 23:17:47 +0200

    First commit

---
+++ src/MarkovChain.cpp
@@ -0,0 +1,12 @@
+
+
+
+#include "MarkovChain.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ src/MarkovManager.cpp
@@ -0,0 +1,12 @@
+﻿// MarkovManager.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "MarkovManager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
